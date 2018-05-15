71b2b461e7946daae20445183fd7376c4d8cfc1b
Lin L Du <lin.l.du@pwc.com>
2018-05-15 17:53:12 +0800

    commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const schedule = require('node-schedule');
+const { exec } = require('child_process');
+
+const rule = '3 * * * * *';
+const j = schedule.scheduleJob(rule, () => {
+  console.log('test');
+});
+
+function gitpush(callback) {
+  exec('npm run git:push', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
+}
+
+gitpush();
