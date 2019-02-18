52c3fa3cd3ad9aecfb6d2aa8c6b517a4298e45f6
mrdulin <novaline.dulin@gmail.com>
2019-02-18 19:19:20 +0800

    add js promise samples

---
+++ src/examples/finally/index.ts
@@ -0,0 +1,16 @@
+import { asyncTask } from '../../lib/async-task';
+
+function publish(message: any, cb: () => any) {
+  return asyncTask(message)
+    .then(() => {
+      console.log(message);
+    })
+    .catch(err => {
+      console.error(err);
+    })
+    .finally(cb);
+}
+
+publish('promise.prototype.finally', () => {
+  console.log('callback');
+});
