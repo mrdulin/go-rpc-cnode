73d014ffc6fe7c630f18b77fc8b95171f5c3c1e5
mrdulin <novaline.dulin@gmail.com>
2019-03-23 12:52:05 +0800

    add sample

---
+++ src/examples/nested-promise-all-in-array/index.ts
@@ -0,0 +1,18 @@
+import { asyncTask } from '../../lib/asyncTask';
+
+const tasks: Array<Promise<any>> = [];
+
+const taskQueue: Promise<any> = Promise.all([asyncTask('task A'), asyncTask('task B')]);
+const taskC = asyncTask('task C');
+
+tasks.concat(taskQueue);
+tasks.push(taskC);
+
+const label = 'task run';
+console.time(label);
+Promise.all([taskC, taskQueue])
+  .then(results => {
+    console.log('results: ', results);
+    console.timeEnd(label);
+  })
+  .catch(console.error);
